88b558d0ccbed8fe1d0b832a71341453ca8efaa2
MinhHoang2003 <hoangd16ptit@gmail.com>
2019-05-14 14:35:51 +0700

    create task info ui

---
+++ app/src/main/java/com/sun/colornotetaking/ui/task/info/TaskInfoActivity.java
@@ -0,0 +1,29 @@
+package com.sun.colornotetaking.ui.task.info;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import com.sun.colornotetaking.R;
+
+public class TaskInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_info);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_task_info_screen, menu);
+        return true;
+    }
+}
